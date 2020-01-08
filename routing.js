9457d14d60c8075fa0ebac523de7e7f874bbce39
rajataggarwal <rajat.nitk@winzogames.com>
2020-01-08 12:02:37 +0530

    ok_done

--- routing.js
+++ routing.js
@@ -6,10 +6,6 @@ const jwt = require('jsonwebtoken');
 const jwt_secret_key='admin_rajat'
 db_client.connect();
 
-
-
-
-
 module.exports=
 {
     get_signup:get_signup,
@@ -17,12 +13,14 @@ module.exports=
     get_login:get_login,
     post_login:post_login,
     get_livecontests:get_livecontests,
-    get_logout:get_logout
+    get_logout:get_logout,
+    get_contest:get_contest
 }
 
 function get_signup(req,res)
 {
-    res.render('signup',{errors:{}});
+    if(req.cookies.user_info)res.redirect('/livecontests');
+    else res.render('signup',{errors:{}});
 }
 
 function post_signup(req,res)
@@ -33,29 +31,24 @@ function post_signup(req,res)
 
     db_client.query("select * from user_registeration where username=$1",[username],function(db_err,db_res)
     {
-        if(db_err)errors.push({error:'Internal Error!.'});
-        else if(db_res.rows.length>0)errors.push({error:'Username Already Taken!.'}); 
-
-        if(errors.length==0)
+        if(db_err){res.render('error404');}
+        else
         {
-            db_client.query("insert into user_registeration(username,password) values($1,$2)",[username,password],function(db_err,db_res)
+            if(db_res.rows.length>0)errors.push({error:'Username Already Taken!.'}); 
+            if(errors.length==0)
             {
-               if(db_err)
-               {
-                   errors.push({error:'Internal Error!.'});
-                   res.render('signup',{errors:errors});   
-               }
-               else res.redirect('/login');
-            });
+                db_client.query("insert into user_registeration(username,password) values($1,$2)",[username,password],function(db_err,db_res)
+                {
+                    if(db_err)res.render('error404');   
+                    else res.redirect('/login');
+                });
+            }
+            else res.render('signup',{errors:errors});
         }
-        else res.render('signup',{errors:errors});
     });
 }
 
-function get_login(req, res) 
-{
-    res.render('login',{errors:{}});
-}
+function get_login(req, res) {res.render('login',{errors:{}});}
 
 function post_login(req,res)
 {
@@ -64,11 +57,7 @@ function post_login(req,res)
     errors=[]
     db_client.query("select * from user_registeration where username=$1 and password=$2",[username,password],function(db_err,db_res)
     {
-        if(db_err)
-        {
-            errors.push({error:'Internal Error!.'});
-            res.render('login',errors);
-        }
+        if(db_err)res.render('error404');
         else
         {
             if(db_res.rows.length!=1)
@@ -79,9 +68,9 @@ function post_login(req,res)
             else 
             {
                 let jwt_token=jwt.sign({username:username},jwt_secret_key);
-                let user_info={username:username,password:password,jwt_token:jwt_token}
+                let user_info={username:username,jwt_token:jwt_token}
                 res.cookie('user_info',user_info);
-                res.redirect('livecontests');
+                res.redirect('/livecontests');
             }
         }
         
@@ -91,20 +80,26 @@ function post_login(req,res)
 
 function get_livecontests(req,res)
 {
-    if(req.cookies.user_info)
+    db_client.query("select * from live_contests",function(db_err,db_res)
     {
-        let livecontests=[];
-        livecontests.push({id:1,name:'Long Challenge'});
-        livecontests.push({id:2,name:'LunchTime'});
-        livecontests.push({id:3,name:'CookOff'});
-        res.render('livecontests',{livecontests:livecontests});
-    }
-    else res.redirect('/login');
+        if(db_err)res.render('error404');
+        else 
+        {
+            let livecontests=db_res.rows;
+            let user_info=req.cookies.user_info;
+            res.render('livecontests',{livecontests:livecontests,user_info:user_info});
+        }
+    });    
 }
      
+function get_contest(req,res)
+{
+    let contest_id=req.params.contest_id;    
+    res.send('Information For Contest '+contest_id);
+}
+
 function get_logout(req,res)
 {
-    console.log('LOGOUT');
     res.clearCookie('user_info');
     res.redirect('/login');
 }
